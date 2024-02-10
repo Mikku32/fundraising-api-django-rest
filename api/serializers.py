31fbae0102b042e71bcf35f01e6dd3fc8dea702b
Mikdhad.k <mikdhadk32@gmail.com>
2024-02-11 00:06:21 +0530

    dtabase updated

--- api/serializers.py
+++ api/serializers.py
@@ -17,6 +17,8 @@ class ProjectSerializer(serializers.ModelSerializer):
 
         
 
+        
+
 class DonationSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     project = ProjectSerializer()
--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from api.views import DonationDetail, DonationList, ProjectDetail, ProjectList, UserDetail, UserList
 
 urlpatterns = [
-    path('Users/', UserList.as_view()),
-    path('Users/<int:pk>/', UserDetail.as_view()),
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
 
-    path('Project/', ProjectList.as_view()),
-    path('Project/<int:pk>/', ProjectDetail.as_view()),
+    path('project/', ProjectList.as_view()),
+    path('project/<int:pk>/', ProjectDetail.as_view()),
 
-    path('Donation/', DonationList.as_view()),
-    path('Donation/<int:pk>/', DonationDetail.as_view()),
+    path('donation/', DonationList.as_view()),
+    path('donation/<int:pk>/', DonationDetail.as_view()),
 
     
 ]
